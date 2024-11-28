0a5c02d70d0af6178653f3da4ff4ff6d82dfa126
ThanhB18059162022 <59827241+ThanhB18059162022@users.noreply.github.com>
2024-11-28 07:28:10 +0000

    add suggest

--- src/db/friend.cypher
+++ src/db/friend.cypher
@@ -1,8 +1,18 @@
 MATCH (u:User) RETURN u LIMIT 25;
 
 // Get all friend of user
-MATCH (n:User {id: $id})-[:FRIEND]->(b:User)
-RETURN b;
+MATCH (u1:User {id: $id})-[:FRIEND]->(u2:User)
+RETURN u2;
+
+// Get friend of friend
+MATCH (user:User {id: $id})-[:FRIEND]->(friend:User)-[:FRIEND]->(friend_of_friend:User)
+WHERE NOT friend_of_friend.id = $id // Exclude the original user
+RETURN DISTINCT friend_of_friend;
+
+// Get friend id
+MATCH (u1:User {id: $id})-[:FRIEND]->(u2:User)
+WHERE u2.id = $friendId
+RETURN u2;
 
 // Add friend
 // MATCH (u1:User {id: $id1})
--- src/services/friends/friends.class.js
+++ src/services/friends/friends.class.js
@@ -1,24 +1,67 @@
 "use strict";
 
-const { session } = require("../../neo4j");
+const { session, neo4j } = require("../../neo4j");
 
 exports.Friends = class Friends {
   // Find all friends or filter by properties
   async find(params) {
     const { query, user } = params;
 
+    if (query.suggestion) {
+      return this.suggestFriends(params);
+    }
+
     const id = query.id || user._id;
 
     const result = await session.run(
       `
-      MATCH (n:User {id: $id})-[:FRIEND]->(b:User)
-      RETURN b;
+      MATCH (u1:User {id: $id})-[:FRIEND]->(u2:User)
+      RETURN u2
+      LIMIT $limit;
       `,
-      { id: id }
+      { id: id, limit: neo4j.int(query.limit || 10) }
     );
 
     // Return friends' properties
-    return result.records.map((record) => record.get("b").properties);
+    return result.records.map((record) => record.get("u2").properties);
+  }
+
+  async suggestFriends(params) {
+    const { query, user } = params;
+    console.log("🚀 ~ Friends ~ suggestFriends ~ query:", query);
+    const userId = query.id || user._id;
+
+    const result = await session.run(
+      `
+      MATCH (user:User {id: $userId})-[:FRIEND]->(friend:User)-[:FRIEND]->(suggestion:User)
+      WHERE NOT (user)-[:FRIEND]->(suggestion) AND suggestion.id <> $userId
+      RETURN DISTINCT suggestion
+      LIMIT $limit;
+      `,
+      { userId: userId, limit: neo4j.int(query.limit || 10) }
+    );
+
+    return result.records.map((record) => record.get("suggestion").properties);
+  }
+
+  // Get a friend by ID
+  async get(id, params) {
+    const { query, user } = params;
+
+    const userId = id || user._id;
+    const { friendId } = query;
+
+    const result = await session.run(
+      `
+      MATCH (u1:User {id: $id})-[:FRIEND]->(u2:User)
+      WHERE u2.id = $friendId
+      RETURN u2;
+      `,
+      { id: userId, friendId: friendId }
+    );
+
+    // Return friends' properties
+    return result.records.map((record) => record.get("u2").properties)[0];
   }
 
   // Create a new friend node
